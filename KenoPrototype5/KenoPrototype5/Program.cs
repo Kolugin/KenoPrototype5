a13dff3d8e18f4a44551185e5641fd3e730f4918
Kolugin <nikita_chizh@mail.ru>
2023-03-28 21:06:40 +0300

    KenoPrototype5

---
+++ KenoPrototype5/KenoPrototype5/Program.cs
@@ -0,0 +1,374 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.InputEncoding = Encoding.Unicode;
+        Console.OutputEncoding = Encoding.Unicode;
+        Console.ForegroundColor = ConsoleColor.DarkRed;
+        int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60};
+        Console.WriteLine("                                ▛▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▜");
+        Console.WriteLine("Тишина! Идет загрузка програмы  ▮=                               ▮");
+        Console.WriteLine("                                ▙▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▟");
+        Thread.Sleep(1000);
+        Console.Clear();
+        Console.WriteLine("                                ▛▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▜");
+        Console.WriteLine("Тишина! Идет загрузка програмы  ▮==                              ▮");
+        Console.WriteLine("                                ▙▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▟");
+        Thread.Sleep(1000);
+        Console.Clear();
+        Console.WriteLine("                                ▛▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▜");
+        Console.WriteLine("Тишина! Идет загрузка програмы  ▮===                             ▮");
+        Console.WriteLine("                                ▙▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▟");
+        Thread.Sleep(1000);
+        Console.Clear();
+        Console.WriteLine("                                ▛▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▜");
+        Console.WriteLine("Тишина! Идет загрузка програмы  ▮====                            ▮");
+        Console.WriteLine("                                ▙▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▟");
+        Thread.Sleep(1000);
+        Console.Clear();
+        Console.WriteLine("                                ▛▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▜");
+        Console.WriteLine("Тишина! Идет загрузка програмы  ▮=====                           ▮");
+        Console.WriteLine("                                ▙▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▟");
+        Thread.Sleep(1000);
+        Console.Clear();
+        Console.WriteLine("                                ▛▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▜");
+        Console.WriteLine("Тишина! Идет загрузка програмы  ▮======                          ▮");
+        Console.WriteLine("                                ▙▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▟");
+        Thread.Sleep(1000);
+        Console.Clear();
+        Console.WriteLine("                                ▛▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▜");
+        Console.WriteLine("Тишина! Идет загрузка програмы  ▮=======                         ▮");
+        Console.WriteLine("                                ▙▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▟");
+        Thread.Sleep(3000);
+        Console.Clear();
+        Console.WriteLine("                                ▛▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▜");
+        Console.WriteLine("Тишина! Идет загрузка програмы  ▮===================             ▮");
+        Console.WriteLine("                                ▙▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▟");
+        Thread.Sleep(1000);
+        Console.Clear();
+        Console.WriteLine("                                ▛▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▜");
+        Console.WriteLine("Тишина! Идет загрузка програмы  ▮=====================           ▮");
+        Console.WriteLine("                                ▙▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▟");
+        Thread.Sleep(1800);
+        Console.Clear();
+        Console.WriteLine("                                ▛▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▜");
+        Console.WriteLine("Тишина! Идет загрузка програмы  ▮===========================     ▮");
+        Console.WriteLine("                                ▙▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▟");
+        Thread.Sleep(1300);
+        Console.Clear();
+        Console.WriteLine("                                ▛▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▜");
+        Console.WriteLine("Тишина! Идет загрузка програмы  ▮=============================== ▮");
+        Console.WriteLine("                                ▙▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▟");
+        Thread.Sleep(1000);
+        Console.Clear();
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("                                ▛▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▝▜");
+        Console.WriteLine("                                ▮  Загрузка программы завершена! ▮");
+        Console.WriteLine("                                ▙▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▖▟");
+        Thread.Sleep(1000);
+        Console.Clear();
+
+        while (true)
+        {
+            Console.WriteLine("Что ищем?");
+            Console.WriteLine("1 - Комбинации 2-х чисел из 60");
+            Console.WriteLine("2 - Комбинации 3-х чисел из 60");
+            Console.WriteLine("3 - Комбинации 4-х чисел из 60");
+            Console.WriteLine("4 - Комбинации 5-ти чисел из 60");
+            Console.WriteLine("5 - Комбинации 6-ти чисел из 60");
+            Console.WriteLine("6 - Комбинации 7-ми чисел из 60");
+            Console.WriteLine("7 - Комбинации 8-ми чисел из 60");
+            Console.WriteLine("8 - Комбинации 9-ти чисел из 60");
+            Console.WriteLine("9 - Комбинации 10-ти чисел из 60");
+            Console.WriteLine("0 - Выйти");
+
+            int choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    Find2(arr);
+                    break;
+                case 2:
+                    Find3(arr);
+                    break;
+                case 3:
+                    Find4(arr);
+                    break;
+                case 4:
+                    Find5(arr);
+                    break;
+                case 5:
+                    Find6(arr);
+                    break;
+                case 6:
+                    Find7(arr);
+                    break;
+                case 7:
+                    Find8(arr);
+                    break;
+                case 8:
+                    Find9(arr);
+                    break;
+                case 9:
+                    Find10(arr);
+                    break;
+                case 0:
+                    return;
+                default:
+                    Console.WriteLine("Неправильный выбор");
+                    break;
+            }
+        }
+    }
+
+    static void Find2(int[] arr)
+    {
+        using (StreamWriter file = new StreamWriter("2.txt")) // открываем файл на запись
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    string comb2 = $"{arr[i]}, {arr[j]}"; // сохраняем в строку
+                    Console.WriteLine(comb2); // выводим на консоль
+                    file.WriteLine(comb2); // записываем в файл
+                }
+            }
+        }
+    }
+
+    static void Find3(int[] arr)
+    {
+        using (StreamWriter file = new StreamWriter("3.txt")) // открываем файл на запись
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    for (int k = j + 1; k < arr.Length; k++)
+                    {
+                        string comb3 = $"{arr[i]}, {arr[j]}, {arr[k]}"; // сохраняем в строку
+                        Console.WriteLine(comb3); // выводим на консоль
+                        file.WriteLine(comb3); // записываем в файл
+                    }
+                }
+            }
+        }
+    }
+    static void Find4(int[] arr)
+    {
+        using (StreamWriter file = new StreamWriter("4.txt")) // открываем файл на запись
+        {
+            for (int i = 0; i < arr.Length - 3; i++)
+            {
+                for (int j = i + 1; j < arr.Length - 2; j++)
+                {
+                    for (int k = j + 1; k < arr.Length - 1; k++)
+                    {
+                        for (int l = k + 1; l < arr.Length; l++)
+                        {
+                            string comb3 = $"{arr[i]}, {arr[j]}, {arr[k]}, {arr[l]}"; // сохраняем в строку
+                            Console.WriteLine(comb3); // выводим на консоль
+                            file.WriteLine(comb3); // записываем в файл
+                        }
+                    }
+                }
+            }
+        }
+    }
+    static void Find5(int[] arr)
+    {
+        using (StreamWriter file = new StreamWriter("5.txt")) // открываем файл на запись
+        {
+            for (int i = 0; i < arr.Length - 4; i++)
+            {
+                for (int j = i + 1; j < arr.Length - 3; j++)
+                {
+                    for (int k = j + 1; k < arr.Length - 2; k++)
+                    {
+                        for (int l = k + 1; l < arr.Length - 1; l++)
+                        {
+                            for (int m = l + 1; m < arr.Length; m++)
+                            {
+                                string comb3 = $"{arr[i]}, {arr[j]}, {arr[k]}, {arr[l]}, {arr[m]}"; // сохраняем в строку
+                                Console.WriteLine(comb3); // выводим на консоль
+                                file.WriteLine(comb3); // записываем в файл
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    static void Find6(int[] arr)
+    {
+        using (StreamWriter file = new StreamWriter("6.txt")) // открываем файл на запись
+        {
+            for (int i = 0; i < arr.Length - 5; i++)
+            {
+                for (int j = i + 1; j < arr.Length - 4; j++)
+                {
+                    for (int k = j + 1; k < arr.Length - 3; k++)
+                    {
+                        for (int l = k + 1; l < arr.Length - 2; l++)
+                        {
+                            for (int m = l + 1; m < arr.Length - 1; m++)
+                            {
+                                for (int n = m + 1; n < arr.Length; n++)
+                                {
+                                    string comb3 = $"{arr[i]}, {arr[j]}, {arr[k]}, {arr[l]}, {arr[m]}, {arr[n]}"; // сохраняем в строку
+                                    Console.WriteLine(comb3); // выводим на консоль
+                                    file.WriteLine(comb3); // записываем в файл
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    static void Find7(int[] arr)
+    {
+        using (StreamWriter file = new StreamWriter("7.txt")) // открываем файл на запись
+        {
+            for (int i = 0; i < arr.Length - 6; i++)
+            {
+                for (int j = i + 1; j < arr.Length - 5; j++)
+                {
+                    for (int k = j + 1; k < arr.Length - 4; k++)
+                    {
+                        for (int l = k + 1; l < arr.Length - 3; l++)
+                        {
+                            for (int m = l + 1; m < arr.Length - 2; m++)
+                            {
+                                for (int n = m + 1; n < arr.Length - 1; n++)
+                                {
+                                    for (int o = n + 1; o < arr.Length; o++)
+                                    {
+                                        string comb3 = $"{arr[i]}, {arr[j]}, {arr[k]}, {arr[l]}, {arr[m]}, {arr[n]}, {arr[o]}"; // сохраняем в строку
+                                        Console.WriteLine(comb3); // выводим на консоль
+                                        file.WriteLine(comb3); // записываем в файл
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    static void Find8(int[] arr)
+    {
+        using (StreamWriter file = new StreamWriter("8.txt")) // открываем файл на запись
+        {
+            for (int i = 0; i < arr.Length - 7; i++)
+            {
+                for (int j = i + 1; j < arr.Length - 6; j++)
+                {
+                    for (int k = j + 1; k < arr.Length - 5; k++)
+                    {
+                        for (int l = k + 1; l < arr.Length - 4; l++)
+                        {
+                            for (int m = l + 1; m < arr.Length - 3; m++)
+                            {
+                                for (int n = m + 1; n < arr.Length - 2; n++)
+                                {
+                                    for (int o = n + 1; o < arr.Length - 1; o++)
+                                    {
+                                        for (int p = o + 1; p < arr.Length; p++)
+                                        {
+                                            string comb3 = $"{arr[i]}, {arr[j]}, {arr[k]}, {arr[l]}, {arr[m]}, {arr[n]}, {arr[o]}, {arr[p]}"; // сохраняем в строку
+                                            Console.WriteLine(comb3); // выводим на консоль
+                                            file.WriteLine(comb3); // записываем в файл
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    static void Find9(int[] arr)
+    {
+        using (StreamWriter file = new StreamWriter("9.txt")) // открываем файл на запись
+        {
+            for (int i = 0; i < arr.Length - 8; i++)
+            {
+                for (int j = i + 1; j < arr.Length - 7; j++)
+                {
+                    for (int k = j + 1; k < arr.Length - 6; k++)
+                    {
+                        for (int l = k + 1; l < arr.Length - 5; l++)
+                        {
+                            for (int m = l + 1; m < arr.Length - 4; m++)
+                            {
+                                for (int n = m + 1; n < arr.Length - 3; n++)
+                                {
+                                    for (int o = n + 1; o < arr.Length - 2; o++)
+                                    {
+                                        for (int p = o + 1; p < arr.Length - 1; p++)
+                                        {
+                                            for (int q = p + 1; q < arr.Length; q++)
+                                            {
+                                                string comb3 = $"{arr[i]}, {arr[j]}, {arr[k]}, {arr[l]}, {arr[m]}, {arr[n]}, {arr[o]}, {arr[p]}, {arr[q]}"; // сохраняем в строку
+                                                Console.WriteLine(comb3); // выводим на консоль
+                                                file.WriteLine(comb3); // записываем в файл
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    static void Find10(int[] arr)
+    {
+        using (StreamWriter file = new StreamWriter("10.txt")) // открываем файл на запись
+        {
+            for (int i = 0; i < arr.Length - 9; i++)
+            {
+                for (int j = i + 1; j < arr.Length - 8; j++)
+                {
+                    for (int k = j + 1; k < arr.Length - 7; k++)
+                    {
+                        for (int l = k + 1; l < arr.Length - 6; l++)
+                        {
+                            for (int m = l + 1; m < arr.Length - 5; m++)
+                            {
+                                for (int n = m + 1; n < arr.Length - 4; n++)
+                                {
+                                    for (int o = n + 1; o < arr.Length - 3; o++)
+                                    {
+                                        for (int p = o + 1; p < arr.Length - 2; p++)
+                                        {
+                                            for (int q = p + 1; q < arr.Length - 1; q++)
+                                            {
+                                                for (int r = q + 1; r < arr.Length; r++)
+                                                {
+                                                    string comb3 = $"{arr[i]}, {arr[j]}, {arr[k]}, {arr[l]}, {arr[m]}, {arr[n]}, {arr[o]}, {arr[p]}, {arr[q]}, {arr[r]}"; // сохраняем в строку
+                                                    Console.WriteLine(comb3); // выводим на консоль
+                                                    file.WriteLine(comb3); // записываем в файл
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
